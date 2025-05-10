fc5a0176975628dfdeb61ffa9b3660a6cfe87e48
EduardoProggt <roadslocker@gmail.com>
2025-05-10 20:32:06 +0200

    Ghost-Walls, ín die überlappt weir, werden entfernt

--- assets/wall.gd
+++ assets/wall.gd
@@ -13,9 +13,26 @@ func display(result: Dictionary):
 		else:
 			transform.origin = collision_shape.global_transform.origin + Vector3(0.5,-3,-3);
 			rotation.y = PI / 2
-	
+			
 func spawn(add_to_scene: Callable):
-	add_to_scene.call(duplicate(DuplicateFlags.DUPLICATE_USE_INSTANTIATION))
+	var new_wall : Node3D = duplicate(DuplicateFlags.DUPLICATE_USE_INSTANTIATION)
+	add_to_scene.call(new_wall)
 	
+	delete_colliding_shapes(new_wall)
+
+func delete_colliding_shapes(new_wall):
+	var space_state = get_world_3d().direct_space_state
+	var shape: CollisionShape3D = new_wall.find_child("CollisionShape3D")
+		
+	var query = PhysicsShapeQueryParameters3D.new()
+	query.shape = shape.shape
+	query.transform = shape.global_transform
+
+	var results = space_state.intersect_shape(query, 32)
+	for res in results:
+		var obj: Node = res.collider
+		if obj.is_in_group("WallCollider"): 
+			obj.queue_free()
+
 func finish_spawning():
 	pass
