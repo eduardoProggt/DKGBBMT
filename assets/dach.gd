359a97f54486cc1cd70f9812fe6bac146756d049
EduardoProggt <roadslocker@gmail.com>
2025-05-24 15:00:01 +0200

    Dach-Ghosts spawnen an der richtigen Stelle

--- assets/dach.gd
+++ assets/dach.gd
@@ -38,10 +38,20 @@ func spawn_gost(node: Connector):
 		var newCollisionBox : CollisionShape3D = collisionBox.duplicate(DuplicateFlags.DUPLICATE_USE_INSTANTIATION)
 		var body = StaticBody3D.new()
 		var shape :BoxShape3D = newCollisionBox.shape
-		#TODO: Rausfinden, warum es genau die x & z Werte sind
-		body.global_transform.origin =  (pos + node.get_center() - shape.size) / 2 - Vector3(0,0,-6)
+		var middle = (pos + node.get_center()) / 2
+		
+		body.global_transform.origin =  (pos + node.get_center() - shape.size) / 2 + Vector3(0,0.6,6) #Der Vector resultiert daraus, dass die Shape 13,0,6 ist aber 13,0,-6 die Position, daher die Halfte *2 = -6 abziehen
 		body.add_child(newCollisionBox)
 		get_tree().current_scene.add_child(body)
 		Utils.spawn_debug_sphere(get_tree(),pos)
 	for pos in relevant_positions_vertical:
+		var newCollisionBox : CollisionShape3D = collisionBox.duplicate(DuplicateFlags.DUPLICATE_USE_INSTANTIATION)
+		newCollisionBox.rotate(Vector3(0,1,0), PI/2)
+		var body = StaticBody3D.new()
+		var shape :BoxShape3D = newCollisionBox.shape
+		var middle = (pos + node.get_center()) / 2
+		
+		body.global_transform.origin =  (pos + node.get_center() - shape.size) / 2 + Vector3(0,0.6,6) #Der Vector resultiert daraus, dass die Shape 13,0,6 ist aber 13,0,-6 die Position, daher die Halfte *2 = -6 abziehen
+		body.add_child(newCollisionBox)
+		get_tree().current_scene.add_child(body)
 		Utils.spawn_debug_sphere(get_tree(),pos)
